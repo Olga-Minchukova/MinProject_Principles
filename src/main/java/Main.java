b9515d1819a51e251f7779cb1916c3823acddce8
Оля М <olsermi@outlook.com>
2023-11-14 16:05:19 +0300

    src/main/java/FormDate.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        Post post = new Post();
+        post.name = "Ольга";
+        post.passport = "1234 № 12345678";
+        post.patronymic = "Сергеевна";
+        post.phone = "+7 (999)-999-99-99";
+        post.surname = "Минчукова";
+        post.subscription = Boolean.parseBoolean("true");
+        post.birthday = new FormDate();
+        post.birthday.day = 11;
+        post.birthday.month = 10;
+        post.birthday.year = 1986;
+    }
 }
